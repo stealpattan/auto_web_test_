e9a892497a999dd15302f15b76c7f5cc3fa48a2c
Ito Shoma <stealpattan@gmail.com>
2020-09-07 19:05:35 +0900

    #3 読み込みの処理も追加

--- src/FeasibilityStudy/FileController.java
+++ src/FeasibilityStudy/FileController.java
@@ -1,9 +1,6 @@
 package FeasibilityStudy;
 
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class FileController {
     public static void main(String[] args) {
@@ -12,6 +9,9 @@ public class FileController {
         // ファイル入出力処理を呼び出してみる
         boolean result = writeInFile(fileName, data);
         System.out.println("file input result: " + result);
+
+        // ファイル読み込み処理を呼び出してみる
+        readFile(fileName);
     }
 
     /**
@@ -26,11 +26,31 @@ public class FileController {
             bw.write(data);
             bw.newLine();
             bw.flush();
+            // ファイルアクセス終了
+            bw.close();
+            // 書き込み成功フラグを立てる
             isSucsess = true;
         } catch(Exception e){
             e.printStackTrace();
         }
-
         return isSucsess;
+    }
+
+    /**
+     * ファイル読み込み処理をする
+     * ファイル名を指定して、全行読み込んでコンソール出力
+     * */
+    public static void readFile(String fileName){
+        // 読み込み処理
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String out;
+            while((out = br.readLine()) != null){
+                System.out.println(out);
+            }
+            br.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
